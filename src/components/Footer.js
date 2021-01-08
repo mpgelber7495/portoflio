0f6fa86d9467a959def5efe34206a5c5227d1433
Michael Gelber <mpgelber7495@gmail.com>
2021-01-08 12:46:16 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,26 @@ class Footer extends Component {
   }
 
   render() {
-    return <Container fluid id="main-body-container"></Container>;
+    return (
+      <Container fluid id="footer">
+        {" "}
+        <div className="ui stackable grid">
+          <div className="eight wide column">
+            <a href="https://github.com/mpgelber7495" target="_blank">
+              <i class="github square icon"></i>
+            </a>
+          </div>
+          <div className="eight wide column">
+            <a
+              href="https://www.linkedin.com/in/michael-gelber-290329a5/"
+              target="_blank"
+            >
+              <i class="linkedin  icon"></i>
+            </a>
+          </div>
+        </div>
+      </Container>
+    );
   }
 }
 
