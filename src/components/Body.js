92fbf371fbb99a9cba54ad7d3c87957be32947fd
Michael Gelber <mpgelber7495@gmail.com>
2021-02-09 13:04:52 -0600

    Styling: Down arrow Float

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,6 +17,16 @@ class Body extends Component {
             Welcome to Mike's Portf <Typing.Backspace count={6} /> Website!
           </h5>
         </Typing>
+        {/* <Typing speed={10}> */}
+        <p className="intro-paragraph">
+          Hi &#128075; I'm Mike Gelber! I'm a Product Manager and Software
+          Engineer who is passionate about building products that solve problems
+          for people. I enjoy learning about technology and working in
+          collaborative environments. Scroll down to learn more!
+          <br />
+          <i className="caret down icon intro-arrow-down"></i>
+        </p>
+        {/* </Typing> */}
       </Container>
     );
   }
